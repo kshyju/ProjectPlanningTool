5f2260adf86571ad0948b5b66361cc01810028f6
Shyju <connectshyju@gmail.com>
2016-08-14 00:00:33 -0400

    Converting some code to use MVC6 tag helpers

--- src/TeamBinsCore.Web/Infrastrucutre/Services/CommentManager.cs
+++ src/TeamBinsCore.Web/Infrastrucutre/Services/CommentManager.cs
@@ -24,10 +24,10 @@ namespace TeamBins.Services
 
     public class CommentManager : ICommentManager
     {
-        private IActivityRepository activityRepository;
-        private ICommentRepository commentRepository;
-        private IIssueRepository issueRepository;
-        private IUserAuthHelper userSessionHelper;
+        private readonly IActivityRepository activityRepository;
+        private readonly ICommentRepository commentRepository;
+        private readonly IIssueRepository issueRepository;
+        private readonly IUserAuthHelper userSessionHelper;
         public CommentManager(ICommentRepository commentRepository,IUserAuthHelper userSessionHelper, IIssueRepository issueRepository, IActivityRepository activityRepository)
         {
             this.commentRepository = commentRepository;
@@ -101,80 +101,4 @@ namespace TeamBins.Services
             throw new NotImplementedException();
         }
     }
-    //public class CommentManager : ICommentManager
-    //{
-    //    IUserRepository userRepository;
-    //    IIssueRepository issueRepository;
-    //    ICommentRepository commentRepository;
-    //    IUserAuthHelper userSessionHelper;
-    //    IActivityRepository activityRepository;
-    //    ICommentEmailManager commentEmailManager;
-    //    public CommentManager(ICommentRepository commentRepository, IIssueRepository issueRepository,
-    //        IActivityRepository activityRepository,
-    //        IUserRepository userRepository,
-    //        ICommentEmailManager commentEmailManager,
-    //        IUserAuthHelper userSessionHelper)
-    //    {
-    //        this.commentRepository = commentRepository;
-    //        this.issueRepository = issueRepository;
-    //        this.userSessionHelper = userSessionHelper;
-    //        this.activityRepository = activityRepository;
-    //        this.userRepository = userRepository;
-    //        this.commentEmailManager = commentEmailManager;
-    //    }
-    //    public IEnumerable<CommentVM> GetComments(int issueId)
-    //    {
-    //        var comments = commentRepository.GetComments(issueId);
-    //        foreach (var commentVm in comments)
-    //        {
-    //            commentVm.IsOwner = commentVm.Author.Id == userSessionHelper.UserId;
-    //            commentVm.Author.GravatarUrl = UserService.GetImageSource(commentVm.Author.EmailAddress, 42);
-    //        }
-    //        return comments;
-    //    }
-
-    //    public CommentVM GetComment(int id)
-    //    {
-    //        var commentVm = commentRepository.GetComment(id);
-    //        commentVm.IsOwner = commentVm.Author.Id == userSessionHelper.UserId;
-    //        commentVm.Author.GravatarUrl = UserService.GetImageSource(commentVm.Author.EmailAddress, 42);
-    //        return commentVm;
-    //    }
-
-    //    public ActivityDto SaveActivity(int commentId, int issueId)
-    //    {
-    //        var issue = issueRepository.GetIssue(issueId);
-    //        if (issue != null)
-    //        {
-    //            var activity = new ActivityDto() {ObjectId = commentId, ObjectType = "Comment"};
-    //            activity.Description = "Commented";
-    //            //activity.NewState = newVersion.Status.Name;
-    //            // activity.OldState = previousVersion.Status.Name;
-    //            activity.ObjectTite = issue.Title;
-    //            activity.TeamId = userSessionHelper.TeamId;
-
-    //            activity.Actor = new UserDto {Id = userSessionHelper.UserId};
-
-
-    //            var newId = activityRepository.Save(activity);
-    //            return activityRepository.GetActivityItem(newId);
-
-    //        }
-    //        return null;
-    //    }
-
-    //    public int SaveComment(CommentVM comment)
-    //    {
-    //        return commentRepository.Save(comment);
-    //    }
-    //    public async Task SendEmailNotificaionForNewComment(CommentVM comment)
-    //    {
-    //        await commentEmailManager.SendNewCommentEmail(comment,userSessionHelper.TeamId);
-    //    }
-
-    //    public async Task Delete(int id)
-    //    {
-    //        await commentRepository.Delete(id);
-    //    }
-    //}
-}
+ }
